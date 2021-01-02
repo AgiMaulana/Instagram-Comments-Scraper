21112c4bfbee535d809f40d4dd8b7142ae7b4058
agimaulana <agimaulana.dex@gmail.com>
2021-01-02 22:40:43 +0700

    detect whether the user was redirected to login page

--- scraper.py
+++ scraper.py
@@ -1,18 +1,61 @@
+import getpass
+import string
+
 from selenium import webdriver
 import time
 import sys
+
+
+def isRedirectedToLoginPage(driver) -> bool:
+    try:
+        username_field = driver.find_element_by_name('username')
+        password_field = driver.find_element_by_name('password')
+        return username_field is not None and password_field is not None
+    except:
+        return False
+
+
+def getUsername() -> string:
+    username = input("Username: ")
+    return username
+
+
+def getPassword() -> string:
+    password = getpass.getpass("Password: ")
+    return password
+
+
+def login(driver, username: string, password: string):
+    username_field = driver.find_element_by_name('username')
+    username_field.sendKeys(username)
+
+    password_field = driver.find_element_by_name('password')
+    password_field.sendKeys(password)
+
+    login_button = driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button')
+    login_button.click()
+
+
+def askLoginCredential(driver):
+    username = getUsername()
+    password = getPassword()
+    print("username: ", username, "password: ", password, end="\n")
+    login(driver, username, password)
+
 
 driver = webdriver.Chrome()
 driver.get(sys.argv[1])
 time.sleep(3)
 
-#if user not logined
+# if user not logged in
 try:
     close_button = driver.find_element_by_class_name('xqRnw')
     close_button.click()
 except:
     pass
 
+if isRedirectedToLoginPage(driver):
+    askLoginCredential(driver)
 
 try:
     load_more_comment = driver.find_element_by_css_selector('.MGdpg > button:nth-child(1)')
@@ -41,7 +84,9 @@ for c in comment:
 
 user_names.pop(0)
 user_comments.pop(0)
+
 import excel_exporter
+
 excel_exporter.export(user_names, user_comments)
 
 driver.close()
